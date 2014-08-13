d09838f075ff64cb332146bb73bbb64d6587a537
Blake Hilscher <blake@hilscher.ca>
2014-08-13 14:37:00 -0400

    compile

--- lib/callpixelsjs/compile.rb
+++ lib/callpixelsjs/compile.rb
@@ -4,15 +4,23 @@ module Callpixelsjs
     class << self
       def perform
         Dir.chdir(root) do |f|
+          run("npm install") unless Dir.exist?('./node_modules')
           # compile src
-          system("grunt")
+          run("grunt")
           output = 'vendor/assets/javascripts/'
           FileUtils.rm_rf(output)
           FileUtils.mkdir_p(output)
           Dir.glob('dist/*.js'){|f| FileUtils.cp( f, output ) }
           # generate jsdocs
-          system("./node_modules/.bin/jsdoc -c config/jsdocs.json")
+          output = 'vendor/documentation/javascripts/'
+          FileUtils.rm_rf(output)
+          run("./node_modules/.bin/jsdoc -c config/jsdocs.json")
         end
+      end
+      
+      def run(command)
+        puts(command)
+        system(command)
       end
       
       def root
