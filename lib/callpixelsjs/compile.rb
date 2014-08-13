71bbb56edf22d6d7492cbff013912a0b3bd05016
Blake Hilscher <blake@hilscher.ca>
2014-08-13 14:02:05 -0400

    compile

--- lib/callpixelsjs/compile.rb
+++ lib/callpixelsjs/compile.rb
@@ -1,18 +1,20 @@
-require "callpixelsjs/rails/version"
-
 module Callpixelsjs
   class Compile
     
     class << self
       def perform
         Dir.chdir(root) do |f|
-          %x{./node_modules/.bin/jsdoc -c config/jsdocs.json}
-          %x{grunt}
-          FileUtils.rm_rf('vendor')
-          FileUtils.mkdir_p('vendor/assets/javascripts/', )
-          Dir.glob('dist/*.js'){|f| FileUtils.cp( f, 'vendor/assets/javascripts/') }
+          # compile src
+          system("grunt")
+          output = 'vendor/assets/javascripts/'
+          FileUtils.rm_rf(output)
+          FileUtils.mkdir_p(output)
+          Dir.glob('dist/*.js'){|f| FileUtils.cp( f, output ) }
+          # generate jsdocs
+          system("./node_modules/.bin/jsdoc -c config/jsdocs.json")
         end
       end
+      
       def root
         File.expand_path(File.join(__FILE__, '../../../'))
       end
